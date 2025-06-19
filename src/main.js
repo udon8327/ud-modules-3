6a4aa2cf5667dbc54a8a654dd670aaaf8508cae3
Bohan <bohan.huang@coolbe.com.tw>
2025-06-19 12:07:31 +0800

    save

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import router from "./router"
 import UdUi from "./components/ud-ui"
-import emitter from "mitt"
+import mitt from 'mitt'
 
 import App from "./App.vue"
 import "./style/all.sass"
 
 const app = createApp(App)
 
-app.config.globalProperties.$mitt = emitter
+app.config.globalProperties.$mitt = mitt();
 
 app
   .use(createPinia())
